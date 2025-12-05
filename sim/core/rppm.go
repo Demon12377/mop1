d356e4c0ed14c38c6249d9d8fe966d0c0e76325e
Adrian Klingen <adrian.klingen@deptagency.com>
2025-12-05 09:58:24 +0100

    Remove ranged haste comment

--- sim/core/rppm.go
+++ sim/core/rppm.go
@@ -34,7 +34,6 @@ type rppmHasteMod struct{}
 
 func (r rppmHasteMod) GetCoefficient(proc *RPPMProc) float64 {
 	// as of 5.2 this should no longer include non 'True haste mods' so only i.E. Lust
-	// We only need to look at Real "Ranged" Haste as it includes all true haste effects
 	return proc.character.TotalRealHasteMultiplier()
 }
 
